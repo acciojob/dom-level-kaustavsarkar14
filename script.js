55b2e45321008ddba6a8eb675bb3a43fb72bfaac
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-18 23:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const level = document.getElementById('level');
+let c = 1;
+        
+while (level.parentNode !== null) {
+  level = level.parentNode; // Update the 'level' variable to its parent node
+  c++;
+}   
+ 
+alert(c);     
